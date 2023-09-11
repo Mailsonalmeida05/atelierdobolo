27b3264bbf3c82078d6a98c393086ae51cec76bc
Mailson Almeida Cerqueira <124589889+Mailsonalmeida05@users.noreply.github.com>
2023-09-11 14:12:45 -0300

    commit init

---
+++ bebidas.js
@@ -0,0 +1,13 @@
+let menuJson = [
+    {id:1, name:'Refrigerante Lata R$ 9,90', img:'icone_boliviano.svg', price:[9.90, 9.90, 9.90], sizes:[' ', '',''], description:'Feito na hora, 250 ML'},
+    {id:2, name:'Suco/Copo R$ 9,90', img:'icone_bolinhocalabresa.svg', price:[9.90, 9.90, 9.90], sizes:[' ', '',''], description:'Feito na hora, 250 ML'},
+    {id:3, name:'Água R$ 12,00', img:'icone_risole.svg', price:[12.00, 12.00, 12.00], sizes:[' ', '',''], description:'Feito na hora, 250 ML '},
+    {id:4, name:'Soda italiana R$ 8888,00', img:'icone_enroladinho.svg', price:[8.00, 8.00, 8.00], sizes:[' ', '',''], description:'Feito na hora, 250 ML '},
+    {id:5, name:'Café Cremoso R$ 8,90', img:'icone_coxinha.svg', price:[8.90, 8.90, 8.90], sizes:[' ', '',''], description:'Feito na hora, 250 ML'},
+    {id:6, name:'Café c/Leite R$ 11,80', img:'icone_coxinha.svg', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'Feito na hora, 250 ML '},
+     {id:7, name:'Cafezinho R$ 11,80', img:'icone_coxinha.svg', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'Feito na hora, 250 ML '},
+      {id:8, name:'Chocolate Quente R$ 11,80', img:'icone_coxinha.svg', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'Feito na hora, 250 ML '},
+   
+ 
+   
+];
---
+++ lanches.js
@@ -0,0 +1,16 @@
+let menuJson = [
+    {id:1, name:'Bolinho De Bacalhau R$ 9,90', img:'icone_boliviano.svg', price:[9.90, 9.90, 9.90], sizes:[' ', '',''], description:'Produtos feitos no dia, 130 Gramas.'},
+    {id:2, name:'Bolinho de carne seca c/ Banana R$ 9,90', img:'icone_bolinhocalabresa.svg', price:[9.90, 9.90, 9.90], sizes:[' ', '',''], description:'Produtos feitos no dia, 130 Gramas.'},
+    {id:3, name:'Bolo Salgado R$ 12,00', img:'icone_risole.svg', price:[12.00, 12.00, 12.00], sizes:[' ', '',''], description:'Produtos feitos no dia, 130 Gramas.'},
+    {id:4, name:'Camarão Capotado R$ 8888,00', img:'icone_enroladinho.svg', price:[8.00, 8.00, 8.00], sizes:[' ', '',''], description:'Produtos feitos no dia, 130 Gramas.'},
+    {id:5, name:'Empada R$ 8,90', img:'icone_coxinha.svg', price:[8.90, 8.90, 8.90], sizes:[' ', '',''], description:'Produtos feitos no dia, 130 Gramas.'},
+    {id:6, name:'Empada doce R$ 11,80', img:'icone_coxinha.svg', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'Produtos feitos no dia, 130 Gramas.'},
+    {id:7, name:'Pão Pizza R$ 11,80', img:'icone_coxinha.svg', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'Produtos feitos no dia, 130 Gramas.'},
+    {id:8, name:'Quiche R$ 11,80', img:'icone_coxinha.svg', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'Produtos feitos no dia, 130 Gramas.'},
+    {id:9, name:'Risole R$ 11,80', img:'icone_coxinha.svg', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'Produtos feitos no dia, 130 Gramas.'},
+    {id:10, name:'Saltenha R$ 11,80', img:'icone_coxinha.svg', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'Produtos feitos no dia, 130 Gramas.'},
+    {id:11, name:'Torta de frango R$ 11,80', img:'icone_coxinha.svg', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'Produtos feitos no dia, 130 Gramas.'},
+   
+ 
+   
+];
